c489a14eeed4580707dbf2bb2f3cd6ff08112b12
Yashwanth Iddibani <yaswanth.iddibani@gmail.com>
2020-04-12 19:47:55 +0100

    JQuery Checks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+// Smooth Scrolling
+$('#navbar a, .btn').click(function(event){
+    if(this.hash != ''){
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -100
+            },
+            500
+        );
+    }
+});
+
+// Sticky Menu Background
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 150){
+        this.document.getElementById('#navbar').style.opacity = 0.3;
+    }else {
+        this.document.getElementById('#navbar').style.opacity = 1;
+    }
+});
+
+// Google Maps 
 var map;
 function initMap() {
     const loc = { lat: 17.432050, lng: 78.372660 };
@@ -7,3 +32,4 @@ function initMap() {
     });
     const marker = new google.maps.Marker({position: loc, map: map});
 }
+
